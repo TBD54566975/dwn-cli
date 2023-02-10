755ae33f369fa2672493924de4726242d137ac6a
Mic Neale <michael.neale@gmail.com>
2023-02-10 15:03:55 +1100

    pick a server from a list

--- src/did/did-loader.js
+++ src/did/did-loader.js
@@ -10,7 +10,13 @@ if (fs.existsSync(config.did.storagePath)) {
   didState = JSON.parse(didStateJson);
 } else {
   if (config.did.method === 'ion') {
-    didState = await DIDIon.generate({ serviceEndpoint: 'https://dwn-aggregator.faktj7f1fndve.ap-southeast-2.cs.amazonlightsail.com/' });
+    const endpoints = [
+                'https://dwn-usa-1.ue8cktdq71va0.us-east-2.cs.amazonlightsail.com/', 
+                'https://dwn-aggregator.faktj7f1fndve.ap-southeast-2.cs.amazonlightsail.com/',
+                'https://dwn-india.vtv94qck5sjvq.ap-south-1.cs.amazonlightsail.com/'
+              ];    
+    const serviceEndpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
+    didState = await DIDIon.generate({ serviceEndpoint: serviceEndpoint });
     fs.writeFileSync(config.did.storagePath, JSON.stringify(didState, null, 2));
   } else {
     throw new Error(`DID Method ${config.did.method} not supported`);
